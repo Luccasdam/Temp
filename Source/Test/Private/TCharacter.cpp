f05cd7b38b98d9ae137af357f0d0d311a7ea0834
Luccasdam <luccas_crims@hotmail.com>
2023-02-17 00:01:29 -0300

    Double Jump
        Re-parent BP_ThirdPersonCharacter to newly created TCharacter class

---
+++ Source/Test/Private/TCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TCharacter.h"
+
+// Sets default values
+ATCharacter::ATCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	JumpMaxCount = 2;
+}
+
+// Called when the game starts or when spawned
+void ATCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Test/Public/TCharacter.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TCharacter.generated.h"
+
+UCLASS()
+class TEST_API ATCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ATCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
